5c5da5a5e2e18600b9dc1b5bb83a0758a59d7d61
HenryGao <yoyohenrygao@163.com>
2024-08-16 20:17:08 +0800

    update docs

--- docs/yasee_ios/demo.md
+++ docs/yasee_ios/demo.md
@@ -2,6 +2,17 @@
 sidebar_position: 1
 ---
 
+
+import ImgText from '@site/src/components/ImgText/ImgText';
+
 # Demo 展示
 --- 
 
+
+
+## 扫描设备 & 绑定设备
+<ImgText width={360} src="/img/绑定设备.gif" text="<h2>步骤解释:</h2>1. 打开App展示的是已绑定设备列表<br><br>2.点击右上角 搜索 按钮进入搜索页面<br><br>3.在搜索到的设备中找到想要连接的设备<br><br>4.点击 链接 按钮 绑定&连接 设备<br><br>5. 已绑列表 已存在绑定设备"/>
+
+
+## 缓存绑定列表
+<ImgText width={360} src="/img/绑定设备.gif" text="<h2>步骤解释:</h2>1. 打开App展示的是已绑定设备列表<br><br>2.点击右上角 搜索 按钮进入搜索页面<br><br>3.在搜索到的设备中找到想要连接的设备<br><br>4.点击 链接 按钮 绑定&连接 设备<br><br>5. 已绑列表 已存在绑定设备" right={true}/>
--- src/components/ImgText/ImgText.tsx
+++ src/components/ImgText/ImgText.tsx
@@ -2,12 +2,17 @@
 
 
 
-export default function ImgText(prop: {src:string,text:string,width:number}) {
+export default function ImgText(prop: {src:string,text:string,width:number,right?:boolean}) {
 
     return (
         <div style={{display:'flex',flexDirection:'row',alignItems:'top',justifyContent:'center',flexWrap:"nowrap"}}>
-            <img src={prop.src} style={{flexGrow:1,width:prop.width}}/>
+            {!(prop.right ?? false) &&
+                <img src={prop.src} style={{flexGrow:1,width:prop.width}}/>
+            }
             <div style={{marginLeft:"20px",flexGrow:1}} dangerouslySetInnerHTML={{__html: prop.text}} />
+            {(prop.right ?? false) &&
+                <img src={prop.src} style={{flexGrow:1,width:prop.width}}/>
+            }
         </div>
     );
 }
