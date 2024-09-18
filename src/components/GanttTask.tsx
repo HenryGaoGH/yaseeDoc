7b8db177bfbd0cf957a878a9ffbb809e6b462ed3
HenryGao <yoyohenrygao@163.com>
2024-09-18 19:44:37 +0800

    update docs

--- src/components/GanttTask.tsx
+++ src/components/GanttTask.tsx
@@ -6,8 +6,8 @@ export function GanttTask() {
 
   let tasks: Task[] = [
       {
-        start: new Date(2024, 9, 18),
-        end: new Date(2024, 10, 11),
+        start: new Date(2024, 8, 18),
+        end: new Date(2024, 9, 11),
         name: '展会突击项目',
         id: 'project',
         type:'project',
@@ -15,8 +15,8 @@ export function GanttTask() {
         isDisabled: true,
         styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
       },{
-        start: new Date(2024, 9, 18),
-        end: new Date(2024, 9, 22),
+        start: new Date(2024, 8, 18),
+        end: new Date(2024, 8, 22),
         name: 'Android 对接工作',
         id: 'task-0',
         project: 'project',
@@ -25,8 +25,8 @@ export function GanttTask() {
         isDisabled: true,
         styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d', backgroundColor: "#8B0000"},
       },{
-        start: new Date(2024, 9, 23),
-        end: new Date(2024, 9, 25),
+        start: new Date(2024, 8, 23),
+        end: new Date(2024, 8, 25),
         name: 'Flutter 插件',
         id: 'task-1',
         project: 'project',
@@ -38,8 +38,8 @@ export function GanttTask() {
           backgroundColor: 'red'
         },
       },{
-        start: new Date(2024, 9, 25),
-        end: new Date(2024, 9, 30),
+        start: new Date(2024, 8, 25),
+        end: new Date(2024, 8, 30),
         name: '百合医|绑定&搜索&蓝牙 功能',
         id: 'task-2',
         project: 'project',
@@ -48,8 +48,8 @@ export function GanttTask() {
         isDisabled: true,
         styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d', backgroundColor: "#FFB6C1" },
       },{
-        start: new Date(2024, 9, 30),
-        end: new Date(2024, 9, 31),
+        start: new Date(2024, 8, 30),
+        end: new Date(2024, 8, 31),
         name: '心电对接',
         id: 'task-3',
         project: 'project',
@@ -58,8 +58,8 @@ export function GanttTask() {
         isDisabled: true,
         styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d', backgroundColor: 'blue' },
       },{
-        start: new Date(2024, 10, 7),
-        end: new Date(2024, 10, 11),
+        start: new Date(2024, 9, 7),
+        end: new Date(2024, 9, 11),
         name: '百合医心电对接',
         id: 'task-4',
         project: 'project',
@@ -68,8 +68,8 @@ export function GanttTask() {
         isDisabled: true,
         styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d', backgroundColor: 'blue' },
       },{
-        start: new Date(2024, 10, 11),
-        end: new Date(2024, 10, 14),
+        start: new Date(2024, 9, 11),
+        end: new Date(2024, 9, 14),
         name: '好络纬Api对接',
         id: 'task-4',
         project: 'project',
@@ -82,6 +82,6 @@ export function GanttTask() {
   ];
 
   return (
-    <Gantt tasks={tasks} locale='zh' listCellWidth='0' ganttHeight={350} />
+    <Gantt tasks={tasks} locale='zh' listCellWidth='0' ganttHeight={350} viewMode={ViewMode.Day} />
   )
 }
