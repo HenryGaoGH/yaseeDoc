057d2db3e178166f4148da2e999ed156a05ba86c
HenryGao <yoyohenrygao@163.com>
2025-02-28 11:15:29 +0800

    update docs

--- src/components/ImgText/ImgText.tsx
+++ src/components/ImgText/ImgText.tsx
@@ -2,14 +2,23 @@
 
 
 
-export default function ImgText(prop: {src:string,text:string,width:number,right?:boolean}) {
+export default function ImgText(prop: {src:string,text:string,width:number,right?:boolean,cotents?:[string]}) {
 
     return (
         <div style={{display:'flex',flexDirection:'row',alignItems:'top',justifyContent:'center',flexWrap:"nowrap"}}>
             {!(prop.right ?? false) &&
                 <img src={prop.src} style={{flexGrow:1,width:prop.width}} />
             }
-            <div style={{marginLeft:"20px",flexGrow:1}} dangerouslySetInnerHTML={{__html: prop.text}} />
+
+            <div style={{flexGrow:1,display:'flex',flexDirection:'column',alignItems:'left'}}>
+                <div style={{marginLeft:"20px"}} dangerouslySetInnerHTML={{__html: prop.text}} />
+                {prop.cotents &&
+                    prop.cotents.map((content) => {
+                        return <div style={{margin:"8px 10px"}} dangerouslySetInnerHTML={{__html: content}} />
+                    })
+                }
+            </div>
+            
             {(prop.right ?? false) &&
                 <img src={prop.src} style={{flexGrow:1,width:prop.width}}/>
             }
